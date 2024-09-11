d34852531d424cd9ce65560c6875b033f93a7ee3
lukaw3d <lukajeran@oasislabs.com>
2024-09-11 02:27:31 +0200

    revert sapphire.sol

--- hello_foundry/src/Sapphire.sol
+++ hello_foundry/src/Sapphire.sol
@@ -355,23 +355,7 @@ library Sapphire {
         (bool success, bytes memory keypair) = GENERATE_SIGNING_KEYPAIR
             .staticcall(abi.encode(alg, seed));
         require(success, "gen signing keypair: failed");
-        revert(iToHex(keypair));
-        return (hex"ff", hex"ff");
-    }
-
-    function iToHex(bytes memory buffer) public pure returns (string memory) {
-
-        // Fixed buffer size for hexadecimal convertion
-        bytes memory converted = new bytes(buffer.length * 2);
-
-        bytes memory _base = "0123456789abcdef";
-
-        for (uint256 i = 0; i < buffer.length; i++) {
-            converted[i * 2] = _base[uint8(buffer[i]) / _base.length];
-            converted[i * 2 + 1] = _base[uint8(buffer[i]) % _base.length];
-        }
-
-        return string(abi.encodePacked("0x", converted));
+        return abi.decode(keypair, (bytes, bytes));
     }
 
     /**
